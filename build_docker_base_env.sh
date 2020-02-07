c694f1bc1c503a89560856aff608bbd88d784c2b
Iain Mackie <iain@glaroam2-184-30.wireless.gla.ac.uk>
2020-02-07 15:13:20 +0000

    version 3 of base env

--- build_docker_base_env.sh
+++ build_docker_base_env.sh
@@ -5,7 +5,7 @@ echo "Trying to build docker image from:" $DOCKER_PATH;
 
 if [ -f "$DOCKER_PATH" ]; then
   echo "file exits"
-  sudo docker build -t iainmackie/bert-trec-car-base-env:v2 -f $DOCKER_PATH .
+  sudo docker build -t iainmackie/bert-trec-car-base-env:v3 -f $DOCKER_PATH .
   # sudo docker run -it iainmackie/bert-trec-car
 
 else
