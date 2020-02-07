e1365116e8aef15ebc1cc7e3f40f73c033e08ecf
Iain Mackie <iain@glaroam2-184-30.wireless.gla.ac.uk>
2020-02-07 14:46:36 +0000

    ready to run with print

--- build_docker_run_eval_simple.sh
+++ build_docker_run_eval_simple.sh
@@ -5,7 +5,7 @@ echo "Trying to build docker image from:" $DOCKER_PATH;
 
 if [ -f "$DOCKER_PATH" ]; then
   echo "file exits"
-  sudo docker build -t iainmackie/bert-trec-car-run-eval-simple:v1 -f $DOCKER_PATH .
+  sudo docker build -t iainmackie/bert-trec-car-run-eval-simple:v2 -f $DOCKER_PATH .
   # sudo docker run -it iainmackie/bert-trec-car
 
 else
